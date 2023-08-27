6f39858662b9f3c4d2398d0e2de6455de34b6496
Julianna Sobczak <julianna.sobczak@gmail.com>
2023-08-27 23:29:51 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    const capitalizedPlayerSelection = 
+        playerSelection.charAt(0).toUpperCase() + 
+        playerSelection.slice(1).toLowerCase();
+    
+    if (capitalizedPlayerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return "Draw";
+        } else if (computerSelection == "Paper") {
+            return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
+        } else {
+            return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
+        }
+    } else if (capitalizedPlayerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
+        } else if (computerSelection == "Paper") {
+            return "Draw";
+        } else {
+            return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
+        }
+    } else if (capitalizedPlayerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
+        } else if (computerSelection == "Paper") {
+            return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
+        } else {
+            return "Draw";
+        }
+    } else {
+        alert(`This is Rock, Paper, Scissors not ${playerSelection}, Paper, Scissors!`);
+    }  
+}
