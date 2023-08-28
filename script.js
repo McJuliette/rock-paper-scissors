1ecd42e2d0a67de9a42dab70e75bb2f2a2e1e345
Julianna Sobczak <julianna.sobczak@gmail.com>
2023-08-28 16:29:44 +0200

    Add game function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore;
+let computerScore;
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
@@ -19,22 +22,28 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Rock") {
             return "Draw";
         } else if (computerSelection == "Paper") {
+            computerScore++;
             return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
         } else {
+            playerScore++;
             return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
         }
     } else if (capitalizedPlayerSelection == "Paper") {
         if (computerSelection == "Rock") {
+            playerScore++;
             return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
         } else if (computerSelection == "Paper") {
             return "Draw";
         } else {
+            computerScore++;
             return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
         }
     } else if (capitalizedPlayerSelection == "Scissors") {
         if (computerSelection == "Rock") {
+            computerScore++;
             return `You Lose! ${computerSelection} beats ${capitalizedPlayerSelection}`;
         } else if (computerSelection == "Paper") {
+            playerScore++;
             return `You Win! ${capitalizedPlayerSelection} beats ${computerSelection}`;
         } else {
             return "Draw";
@@ -42,4 +51,24 @@ function playRound(playerSelection, computerSelection) {
     } else {
         alert(`This is Rock, Paper, Scissors not ${playerSelection}, Paper, Scissors!`);
     }  
+}
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+
+    while(playerScore < 5 && computerScore < 5 ) {
+        const playerSelection = prompt('Rock, Paper, Scissors', 'Rock');
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        console.log(roundResult);
+        console.log(`You: ${playerScore} Computer: ${computerScore}`);
+    }
+
+    if (playerScore === 5) {
+        return('You won the game!')
+    } else {
+        return('You lost the game!')
+    }
 }
